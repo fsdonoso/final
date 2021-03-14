c7728f604d6b98b0dfbe22a6ca6e207d8a208aa2
Codeempowers <codeempowers@gmail.com>
2021-03-13 22:38:29 -0600

    Watchlisted functionality started

--- landing.js
+++ landing.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
         email : user.email
       });
       //Show Current user on screen
-      let = loggedinUser = user.displayName
+      let loggedinUser = user.displayName
       
       document.querySelector('#usr').insertAdjacentHTML('beforeend', `
       <span class="font-bold text-xl">Welcome back, ${loggedinUser}!</span>
@@ -50,7 +50,6 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
 
     // Sign-out button 
-      
       document.querySelector('.sign-in-or-sign-out').innerHTML = `
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Out</button>`
       document.querySelector('.sign-in-or-sign-out').addEventListener('click', function(event) {
@@ -58,6 +57,57 @@ firebase.auth().onAuthStateChanged(async function(user) {
       firebase.auth().signOut()
       document.location.href = 'index.html'
       })
+
+
+
+
+
+          // In the following started working on Watch-list. Missing logic to continue: 
+      // a.) User-specifc watchlist collection needs to be set-uo (as in HW7) and changes made above 
+      // b.) When user is logged in it needs to show the watchlist of stocks - rn only coded for stocks to be added 
+      // c.) When stock appears in Watchlist, the Stockname and price needs to be pulled in ---> might need to change order of code because that is only defined below currenlty  
+      // d.) Functionality for stocks to be un-watchlisted in the watchlist (the) 
+
+
+      // Get data of user specific watch-listed Stocks - TO BE MADE USER-SPECIFIC
+      let snapshotWatchlisted = await db.collection('watchlisted').get()
+      console.log(`Number of watched movies: ${snapshotWatchlisted.size}`)
+      let watchlistedStocks = snapshotWatchlisted.docs
+      // console.log(watchlistedStocks)
+
+      //  Add event listener to the watch-list button to 
+          document.querySelector('watchlist-button').addEventListener('click', async function(event) {
+              event.preventDefault()
+              console.log('watchlist button was clicked')   
+              
+              // 1.) add stock to user-specific watchlist collection in firestore when watchlist-button is clicked  
+              // TBD - TO BE MADE USER-SPECIFIC
+
+
+              // 2.) Show the added stock on the page in the watchlist - - TO BE MADE USER-SPECIFIC
+              document.querySelector(`watchlist-element`).insertAdjacentHTML('beforeend', `
+                                <div class="md:w-full xl:w-1/3">
+                                <div class="bg-white rounded-md flex flex-1 items-center p-4">
+
+                                <div class="xl:px-3">
+                                      <button id="unwatch-userId-markedId" class="bg-yellow-500 hover:bg-yellow-200 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+                                                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-300" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                                  </svg>
+                                      </button>
+                                </div>
+                                    
+                                <div class="w-full xl:px-3">
+                                                <p id="stock-userId-markedId" class="font-semibold text-xl">STOCK NAME</p>
+                                                </div>
+                                                <span id="price-userId-markedId" class="text-blue-500 font-semibold text-lg">$233.78</span>
+                                              </div>
+                                </div>        
+              `)
+           })
+    
+  
+
 
 
 
@@ -83,11 +133,11 @@ firebase.auth().onAuthStateChanged(async function(user) {
   });
   
   window.addEventListener('DOMContentLoaded', function() {
-    // YOUR CODE
-  
+
     let selExchange = document.querySelector("#slExchange");
     let selTickers  = document.querySelector("#tickername");
     let btnCalc     = document.querySelector("#calc");
+
 
     //Event Listener to change Exchange/Market/Country
     selExchange.addEventListener('change', async function(event) {
@@ -122,11 +172,12 @@ firebase.auth().onAuthStateChanged(async function(user) {
       var elt        = document.getElementById('slExchange');
       var optionCurr    = elt.options[elt.selectedIndex].getAttribute("data-curr");
       var optionExch    = elt.options[elt.selectedIndex].getAttribute("data-acro");
-      
       var nomtick = this.options[this.selectedIndex].value;
+
       document.querySelector('#ticker').innerHTML = nomtick;
       document.querySelector('#exchange').innerHTML = optionExch;
       document.querySelector('#currency').innerHTML = optionCurr;
+
       
       //Get nomtick/acronym to call API Endpoint of the latest price/date
       let response = await fetch(`http://api.marketstack.com/v1/eod/latest?access_key=${apiKey}&symbols=${nomtick}`)
@@ -147,7 +198,9 @@ firebase.auth().onAuthStateChanged(async function(user) {
       let inidate = document.querySelector('#dtinidate');
       let enddate = document.querySelector('#dtenddate');
       var nomtick = document.querySelector('#tickername').value;
-
+      // console.log(inidate)
+      // console.log(enddate)
+      
       if(inidate.value != "" && enddate.value != ""){
         strIniDate = new Date(inidate.value.toString());
         strEndDate = new Date(enddate.value.toString());
@@ -176,11 +229,15 @@ firebase.auth().onAuthStateChanged(async function(user) {
         chnge =  Math.round((chnge*100)*100) / 100;
         avg_tick = Math.round((avg_tick / hdeod.length) * 100) / 100;
         
+        console.log(chnge)
+
         document.querySelector('#avg').innerHTML = "$" + avg_tick;
         document.querySelector('#perchange').innerHTML = chnge+"%";
         document.querySelector('#min').innerHTML = "$" + min;
         document.querySelector('#max').innerHTML = "$" + max;
         
+
+
         return false;
       }else{
         alert("Please pick a date range");
